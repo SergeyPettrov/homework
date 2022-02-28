99e237347d2f22349b75f39cade9e873a4d8e505
SergeyPettrov <jwock@yandex.ru>
2022-02-28 13:52:03 +0300

    hw1,2

---
+++ hw02/2_hw_select_group.sql
@@ -0,0 +1,235 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+Занятие "02 - Оператор SELECT и простые фильтры, GROUP BY, HAVING".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Все товары, в названии которых есть "urgent" или название начинается с "Animal".
+Вывести: ИД товара (StockItemID), наименование товара (StockItemName).
+Таблицы: Warehouse.StockItems.
+*/
+
+select StockItemID, StockItemName
+from Warehouse.StockItems
+where StockItemName like '%urgent%'
+
+/*
+2. Поставщиков (Suppliers), у которых не было сделано ни одного заказа (PurchaseOrders).
+Сделать через JOIN, с подзапросом задание принято не будет.
+Вывести: ИД поставщика (SupplierID), наименование поставщика (SupplierName).
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders.
+По каким колонкам делать JOIN подумайте самостоятельно.
+*/
+
+select distinct ps.SupplierID, ps.SupplierName
+from Purchasing.Suppliers as ps
+left join Purchasing.PurchaseOrders as pp
+	on ps.SupplierID = pp.SupplierID
+	where pp.PurchaseOrderID is null
+
+/*
+3. Заказы (Orders) с ценой товара (UnitPrice) более 100$ 
+либо количеством единиц (Quantity) товара более 20 штук
+и присутствующей датой комплектации всего заказа (PickingCompletedWhen).
+Вывести:
+* OrderID
+* дату заказа (OrderDate) в формате ДД.ММ.ГГГГ
+* название месяца, в котором был сделан заказ
+* номер квартала, в котором был сделан заказ
+* треть года, к которой относится дата заказа (каждая треть по 4 месяца)
+* имя заказчика (Customer)
+Добавьте вариант этого запроса с постраничной выборкой,
+пропустив первую 1000 и отобразив следующие 100 записей.
+
+Сортировка должна быть по номеру квартала, трети года, дате заказа (везде по возрастанию).
+
+Таблицы: Sales.Orders, Sales.OrderLines, Sales.Customers.
+*/
+
+select sp.OrderID
+	,sp.OrderDate
+	,datename(m, sp.OrderDate) as [Month]
+	,datename(q, sp.OrderDate) as [Quarter]
+	,(case
+		when cast(month(sp.OrderDate) as int) <= 4 then 1
+		when cast(month(sp.OrderDate) as int) > 4 and cast(month(sp.OrderDate) as int) <= 8 then 2
+		when cast(month(sp.OrderDate) as int) > 8 then 3
+	end) as Third
+	,sc.CustomerName as Customer
+from Sales.Orders as sp
+	left join Sales.OrderLines as ol
+		on sp.OrderId = ol.OrderID
+	left join Sales.Customers sc
+		on sp.CustomerID = sc.CustomerID
+--пропуск строк
+order by sp.OrderDate, [Quarter], Third asc
+offset 1000 rows fetch first 100 rows only
+
+/*
+4. Заказы поставщикам (Purchasing.Suppliers),
+которые должны быть исполнены (ExpectedDeliveryDate) в январе 2013 года
+с доставкой "Air Freight" или "Refrigerated Air Freight" (DeliveryMethodName)
+и которые исполнены (IsOrderFinalized).
+Вывести:
+* способ доставки (DeliveryMethodName)
+* дата доставки (ExpectedDeliveryDate)
+* имя поставщика
+* имя контактного лица принимавшего заказ (ContactPerson)
+
+Таблицы: Purchasing.Suppliers, Purchasing.PurchaseOrders, Application.DeliveryMethods, Application.People.
+*/
+
+select ad.DeliveryMethodName
+	,pp.ExpectedDeliveryDate
+	,ps.SupplierName
+	,ap.FullName
+from Purchasing.Suppliers as ps
+	join Purchasing.PurchaseOrders as pp
+		on ps.SupplierID = pp.SupplierID
+	join Application.DeliveryMethods as ad
+		on pp.DeliveryMethodID = ad.DeliveryMethodID
+	join Application.People as ap
+		on pp.ContactPersonID = ap.PersonID
+where pp.ExpectedDeliveryDate between '2013-01-01' and '2013-01-31'
+	and ad.DeliveryMethodName like 'Air Freight' or ad.DeliveryMethodName like 'Refrigerated Air Freight'
+	and pp.IsOrderFinalized = 1
+		
+/*
+5. Десять последних продаж (по дате продажи) с именем клиента и именем сотрудника,
+который оформил заказ (SalespersonPerson).
+Сделать без подзапросов.
+*/
+
+select top 10 OrderID
+	,sc.CustomerName
+	,ap.FullName
+from sales.Orders as o
+join Application.People as ap
+	on o.SalespersonPersonID = ap.PersonID
+join Sales.Customers as sc
+	on o.CustomerID = sc.CustomerID
+	order by o.OrderDate desc
+
+/*
+6. Все ид и имена клиентов и их контактные телефоны,
+которые покупали товар "Chocolate frogs 250g".
+Имя товара смотреть в таблице Warehouse.StockItems.
+*/
+
+select sc.CustomerID
+	,sc.CustomerName
+	,sc.PhoneNumber
+from Sales.Customers sc
+	join Sales.Orders as so
+		on sc.CustomerID = so.CustomerID
+	join Sales.OrderLines as sol
+		on so.OrderID = sol.OrderID
+	join Warehouse.StockItems as ws
+		on sol.StockItemID = ws.StockItemID
+		where ws.StockItemName like 'Chocolate frogs 250g'
+
+/*
+7. Посчитать среднюю цену товара, общую сумму продажи по месяцам
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Средняя цена за месяц по всем товарам
+* Общая сумма продаж за месяц
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select 
+	month(si.InvoiceDate) as month
+	,year(si.InvoiceDate) as year
+	,sl.UnitPrice * sl.Quantity as TotalPrice
+	,sum(sl.UnitPrice * sl.Quantity)/sum(sl.Quantity) as avgPrice
+from Sales.Invoices as si
+	join Sales.InvoiceLines as sl
+		on si.InvoiceID = sl.InvoiceID
+	join Warehouse.StockItems as ws
+		on sl.StockItemID = ws.StockItemID
+		group by month(si.InvoiceDate), year(si.InvoiceDate), sl.UnitPrice, sl.Quantity, ws.StockItemName
+		order by month(si.InvoiceDate), year(si.InvoiceDate)
+
+/*
+8. Отобразить все месяцы, где общая сумма продаж превысила 10 000
+
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select 
+	month(si.InvoiceDate) as month
+	,year(si.InvoiceDate) as year
+	,sl.UnitPrice * sl.Quantity as TotalPrice
+from Sales.Invoices as si
+	join Sales.InvoiceLines as sl
+		on si.InvoiceID = sl.InvoiceID
+	join Warehouse.StockItems as ws
+		on sl.StockItemID = ws.StockItemID
+		group by month(si.InvoiceDate), year(si.InvoiceDate), sl.UnitPrice, sl.Quantity
+		having (sl.UnitPrice * sl.Quantity) > 10000
+		order by month(si.InvoiceDate), year(si.InvoiceDate)
+
+/*
+9. Вывести сумму продаж, дату первой продажи
+и количество проданного по месяцам, по товарам,
+продажи которых менее 50 ед в месяц.
+Группировка должна быть по году,  месяцу, товару.
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Наименование товара
+* Сумма продаж
+* Дата первой продажи
+* Количество проданного
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select 
+	month(si.InvoiceDate) as month
+	,year(si.InvoiceDate) as year
+	,ws.StockItemName
+	,sl.Quantity as TotalQuantity
+	,sum(sl.UnitPrice * sl.Quantity) as Sum
+	,sl.UnitPrice
+	,min(InvoiceDate) as firstDate
+from Sales.Invoices as si
+	join Sales.InvoiceLines as sl
+		on si.InvoiceID = sl.InvoiceID
+	join Warehouse.StockItems as ws
+		on sl.StockItemID = ws.StockItemID
+		group by month(si.InvoiceDate), year(si.InvoiceDate), sl.UnitPrice, sl.Quantity, ws.StockItemName
+		having sl.Quantity < 50
+		order by month(si.InvoiceDate), year(si.InvoiceDate)
+
+-- ---------------------------------------------------------------------------
+-- Опционально
+-- ---------------------------------------------------------------------------
+/*
+Написать запросы 8-9 так, чтобы если в каком-то месяце не было продаж,
+то этот месяц также отображался бы в результатах, но там были нули.
+*/
---
+++ hw03/3_hw_subqueries.sql
@@ -0,0 +1,169 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+
+Занятие "03 - Подзапросы, CTE, временные таблицы".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- Для всех заданий, где возможно, сделайте два варианта запросов:
+--  1) через вложенный запрос
+--  2) через WITH (для производных таблиц)
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), 
+и не сделали ни одной продажи 04 июля 2015 года. 
+Вывести ИД сотрудника и его полное имя. 
+Продажи смотреть в таблице Sales.Invoices.
+*/
+
+select PersonID, FullName from Application.People
+where IsSalesperson = 1 
+and PersonID not in (select distinct SalespersonPersonID from Sales.Invoices where InvoiceDate = '2015-07-04')
+
+/*
+2. Выберите товары с минимальной ценой (подзапросом). Сделайте два варианта подзапроса. 
+Вывести: ИД товара, наименование товара, цена.
+*/
+
+select StockItemID, StockItemName, UnitPrice
+from Warehouse.StockItems
+where UnitPrice = (select min(UnitPrice) 
+	from Warehouse.StockItems)
+
+select StockItemID, StockItemName, UnitPrice 
+from Warehouse.StockItems
+where UnitPrice <= all (select UnitPrice 
+	from Warehouse.StockItems)
+
+/*
+3. Выберите информацию по клиентам, которые перевели компании пять максимальных платежей 
+из Sales.CustomerTransactions. 
+Представьте несколько способов (в том числе с CTE). 
+*/
+
+--1
+with cte as (
+	select top 5 TransactionAmount
+		,CustomerID
+	from Sales.CustomerTransactions
+	order by TransactionAmount desc
+	)
+
+select * from Sales.Customers as sc
+	join cte as c
+		on sc.CustomerID = c.CustomerID
+
+--2
+select * from Sales.Customers as sc
+join(
+	select top 5 TransactionAmount, CustomerID 
+	from Sales.CustomerTransactions 
+	order by TransactionAmount desc
+	) as c
+		on sc.CustomerID = c.CustomerID
+
+--3
+select * from Sales.Customers
+where CustomerID in (select top 5 CustomerID 
+	from Sales.CustomerTransactions 
+	order by TransactionAmount desc)
+
+/*
+4. Выберите города (ид и название), в которые были доставлены товары, 
+входящие в тройку самых дорогих товаров, а также имя сотрудника, 
+который осуществлял упаковку заказов (PackedByPersonID).
+*/
+
+with cte as(
+	select distinct DeliveryCityID, StockItemID, PackedByPersonID
+	from Sales.Customers as sc
+		join sales.Invoices as si
+			on sc.CustomerID = si.CustomerID
+		join Sales.InvoiceLines as sal
+			on sal.InvoiceID = si.InvoiceID
+		join Application.Cities as ac
+			on ac.CityID = sc.DeliveryCityID
+		join Application.People as ap
+			on ap.PersonID = si.PackedByPersonID
+	where StockItemID in (select top 3 StockItemID from Warehouse.StockItems order by UnitPrice desc)
+			)
+
+select CityID, CityName from Application.Cities where CityID in (select distinct DeliveryCityID from cte)
+union all
+select null, FullName from Application.People where PersonID in (select distinct PackedByPersonID from cte)
+
+-- ---------------------------------------------------------------------------
+-- Опциональное задание
+-- ---------------------------------------------------------------------------
+-- Можно двигаться как в сторону улучшения читабельности запроса, 
+-- так и в сторону упрощения плана\ускорения. 
+-- Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. 
+-- Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). 
+-- Напишите ваши рассуждения по поводу оптимизации. 
+
+-- 5. Объясните, что делает и оптимизируйте запрос
+
+SELECT 
+	Invoices.InvoiceID, 
+	Invoices.InvoiceDate,
+	(SELECT People.FullName
+		FROM Application.People
+		WHERE People.PersonID = Invoices.SalespersonPersonID
+	) AS SalesPersonName,
+	SalesTotals.TotalSumm AS TotalSummByInvoice, 
+	(SELECT SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice)
+		FROM Sales.OrderLines
+		WHERE OrderLines.OrderId = (SELECT Orders.OrderId 
+			FROM Sales.Orders
+			WHERE Orders.PickingCompletedWhen IS NOT NULL	
+				AND Orders.OrderId = Invoices.OrderId)	
+	) AS TotalSummForPickedItems
+FROM Sales.Invoices 
+	JOIN
+	(SELECT InvoiceId, SUM(Quantity*UnitPrice) AS TotalSumm
+	FROM Sales.InvoiceLines
+	GROUP BY InvoiceId
+	HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
+		ON Invoices.InvoiceID = SalesTotals.InvoiceID
+ORDER BY TotalSumm DESC
+
+-- подзапрос в строке (123) получает полное имя продаавца
+-- подзапрос в строке (128) вычисляет общую сумму выбранных товаров при этом еще один подзапрос (130) выбирает id заказов для которых выставлен счет и которые собраны
+-- подзапрос в строке (137) независимый, он получает id счета и общую сумму заказа где общая сумма больше 27000
+
+select i.InvoiceID
+	,i.InvoiceDate,
+	(select p.FullName
+		from Application.People as p
+		where p.PersonID = i.SalespersonPersonID
+	) as SalesPersonName
+	,SalesTotals.TotalSumm as TotalSummByInvoice, 
+	(select sum(ol.PickedQuantity * ol.UnitPrice)
+		from Sales.OrderLines ol
+		where ol.OrderId = o.OrderId) as TotalSummForPickedItems
+from Sales.Invoices as i
+	join Sales.Orders as o
+		on i.OrderID = o.OrderID 
+	and o.PickingCompletedWhen is not null	
+	and o.OrderId = i.OrderId
+join
+	(select InvoiceId, sum(Quantity*UnitPrice) as TotalSumm
+	from Sales.InvoiceLines
+	group by InvoiceId
+	having sum(Quantity*UnitPrice) > 27000) as SalesTotals
+		on i.InvoiceID = SalesTotals.InvoiceID
+order by TotalSumm desc
